c2edd15931b488372de96280c165253c2d157303
joaomiguelreis <reis.miguel.joao@gmail.com>
2019-07-11 12:58:19 -0700

    initialise 2 regions, writing-reading and reading writing each

--- Trial9/read_write_VectorXd_exchange.cc
+++ Trial9/read_write_VectorXd_exchange.cc
@@ -92,13 +92,14 @@ void top_level_task(const Task *task,
 	cout << " " << endl;
 
 	//
-	IndexTaskLauncher task2_launcher(READING_TASK_ID, is, TaskArgument(NULL,0), arg_map);
+	IndexTaskLauncher task2_launcher(READING_TASK_ID, is, TaskArgument(&num_pieces,sizeof(int)), arg_map);
 	RegionRequirement reg1RO_req(reg1_lp, 0, READ_ONLY, EXCLUSIVE, reg1_lr);
 	//RegionRequirement reg2WO_req(reg2_lp, 0, WRITE_ONLY, EXCLUSIVE, reg2_lr);
 	task2_launcher.add_region_requirement(reg1RO_req);
 	task2_launcher.add_region_requirement(reg2WO_req);
 	task2_launcher.region_requirements[0].add_field(0,REG1_ID);
 	task2_launcher.region_requirements[1].add_field(0,REG2_ID);
+	runtime->execute_index_space(ctx, task2_launcher);
 
 	cout << "finished" << endl;
 
@@ -110,7 +111,7 @@ void init_reg(const Task *task,
               const std::vector<PhysicalRegion> &regions,
               Context ctx, Runtime *runtime){
 
-	cout << "WRITING ON REG 2" << endl;
+	cout << "Initialising" << endl;
 
 	const FieldAccessor<WRITE_ONLY, double, 1> acc_reg1(regions[0], REG1_ID);
 	const FieldAccessor<WRITE_ONLY, double, 1> acc_reg2(regions[1], REG2_ID);
@@ -124,13 +125,13 @@ void init_reg(const Task *task,
 	cout << "-    index_point " << point << 
 	     " with index " << index << endl;
 
-	//cout << "num_pieces " << num_pieces << endl;
+     int num_pieces = *((const int*)task->args);
 
 
 
-	if (index==0) acc_reg2[point] = 0;
-	else acc_reg2[point] = index;
-	acc_reg1[point] = 0;
+	if (index==0 || index==num_pieces-1) acc_reg1[point] = 1;
+	else acc_reg1[point] = 0;
+	acc_reg2[point] = index;
 
 	cout << "-    acc_reg1[point] " << acc_reg1[point] << " "
 	     << "-    acc_reg2[point] " << acc_reg2[point] << endl;
@@ -147,17 +148,16 @@ void reg1_task(const Task *task,
 	const FieldAccessor<WRITE_ONLY, double, 1> acc_reg1(regions[0], REG1_ID);
 	const FieldAccessor<READ_ONLY, double, 1> acc_reg2(regions[1], REG2_ID);
 
-	int num_pieces = *((const int*)task->args);
+	
 
 
 	DomainPoint point = task->index_point;
 	int index = point.point_data[0];
 
 	VectorXd U = VectorXd::Ones(5);
-	if (index==0) U *= acc_reg2[num_pieces-1];
-	else U *= acc_reg2[index-1];
+	U *= acc_reg2[index];
 
-	acc_reg1[index] = U(3);
+	acc_reg1[index] = U(index);
 
 	cout << "-    index " << index << endl;
 	cout << "-    acc_reg1[point] " << acc_reg1[index] << " "
@@ -169,6 +169,27 @@ void reg1_task(const Task *task,
 void reg2_task(const Task *task,
                   const std::vector<PhysicalRegion> &regions,
                   Context ctx, Runtime *runtime){
+
+	cout << "WRITING ON REG 2" << endl;
+
+	const FieldAccessor<READ_ONLY, double, 1> acc_reg1(regions[0], REG1_ID);
+	const FieldAccessor<WRITE_ONLY, double, 1> acc_reg2(regions[1], REG2_ID);
+
+	DomainPoint point = task->index_point;
+	int index = point.point_data[0];
+
+	int num_pieces = *((const int*)task->args);
+
+	if (index==0 || index==num_pieces-1){
+		if (index==0) acc_reg2[index] = acc_reg1[num_pieces-1];
+		if (index==num_pieces-1) acc_reg2[index-1] = acc_reg1[index-1];
+	}
+	else acc_reg2[index] = acc_reg1[index-1];
+
+
+	cout << "-    index " << index << endl;
+	cout << "-    acc_reg1[point] " << acc_reg1[index] << " "
+	     << "acc_reg2[point] " << acc_reg2[index] << endl;
 	
 }
 
